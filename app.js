1a1e906dfd95818e2c67738f34d33ee5a7620de3
GolgothaAksanti <38277247+GolgothaAksanti@users.noreply.github.com>
2020-10-16 14:33:04 +0300

    ESLint Installation and setting it up

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 2716
+/* eslint-disable no-console */
+/* eslint linebreak-style: ["error", "windows"] */
+const express = require('express');
 
-const app = express()
+const PORT = process.env.PORT || 2716;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => res.send('Hello World'));
 
-app.use(express.urlencoded({extended: true}))
-
-app.get('/', (req, res) => res.send('Hello World'))
-
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => { console.log(`Server started on http://localhost:${PORT}`); });
